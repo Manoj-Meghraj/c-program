01bf5e4962dc64369bd4673fffb13ab507175561
MeGh <manojpoudel57@gmail.com>
2024-12-16 16:08:51 +0545

    Practice 20
    Using strlen

---
+++ strlen.c
@@ -0,0 +1,13 @@
+// Testing the string length function (strlen)
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+	char a[15];
+	int x;
+	printf("Enter any word whose length is to be known ");
+	scanf("%s",a);
+	x=strlen(a);
+	printf("The string has %d length ",x);
+	return 0;
+}
